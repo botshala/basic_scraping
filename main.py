75d53514d8cc26a76d0b8dabb716650343172c05
Saurav Tomar <stomatrix@gmail.com>
2016-08-07 15:28:00 +0530

    first blood

---
+++ main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib2
+from bs4 import BeautifulSoup
+import os
+import logging
+from soupselect import select
+import re
+import json
+
+def scrape_generic(url = 'http://www.iemoji.com/meanings-gallery/people'):
+    arr =[]
+    soup = BeautifulSoup( urllib2.urlopen(url).read() )
+    for i in soup.find_all('div',{'class' : 'thumbnail'}):
+        try:
+            a = i.find('a')
+            text =  a.get_text()
+            print text
+            link = a.get('href')
+            emoji = scrape_link(link)
+            if not emoji:
+                continue
+
+            arr.append([emoji,text])
+        except:
+            continue
+
+    return arr
+
+def scrape_link(url='http://www.iemoji.com/view/emoji/1/people/smiling-face-with-open-mouth-and-smiling-eyes'):
+    url = 'http://www.iemoji.com/view/emoji/1/people/smiling-face-with-open-mouth-and-smiling-eyes'
+    soup = BeautifulSoup( urllib2.urlopen(url).read() )
+    for i in soup.find_all('input',{'id' : 'code'}):
+        try:
+            return i.get('value')
+        except:
+            return None
+
+def main():
+    tags = 'people;places;nature;objects;symbols;new;skin-tones'.split(';')
+    master_arr = []
+    for tag in tags:
+        master_arr = master_arr + scrape_generic(url='http://www.iemoji.com/meanings-gallery/'+tag)
+    f = open('output.json', 'w')
+    f.write(json.dumps(master_arr))
+
+def render():
+    f = open('output.json','r')
+    a = json.loads(f.read())
+    arr =[]
+    for i in a:
+        arr.append([i[0].decode('utf-8','ignore'),i[1]])
+
+    
+    g = open('b_output.json','w')
+    g.write(json.dumps(arr))
+
+if __name__ == '__main__':
+    #render()
+    #scrape_link()
+    main()
+    #scrape_generic()
+
+    
+
